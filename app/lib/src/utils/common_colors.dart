275e77829d62674dd074d209c1e7500de3eac01c
Tam Tran <>
2024-01-22 23:36:32 +0700

    * config source

---
+++ app/lib/src/utils/common_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CommonColors {
+  static const colorWhite =  Color(0xFFFFFFFF);
+  static const colorRed = Color(0xFFFA6650);
+  static const colorBlue = Color(0xFF2F80ED);
+  static const colorBlack = Color(0xFF000000);
+}
